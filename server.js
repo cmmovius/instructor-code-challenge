1ca5ac1e7518a74cdfa185ee620f1fc51ef1482c
Christine Movius <christine.movius@gmail.com>
2016-04-19 18:02:01 -0400

    Comment out code to focus on one error at a time. App is now listening to localhost:3000 properly.

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 var express = require('express');
+// var fs = require('fs');
+// var path = require('path');
+
 var app = express();
-var fs = require('fs');
-var path = require('path');
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.set("port", process.env.PORT || 3000);
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+// app.use(express.static(path.join(__dirname, '/public')));
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json());
+//
+// app.use('/', express.static(path.join(__dirname, 'public'));
+//
+// app.get('/favorites', function(req, res){
+//   var data = fs.readFileSync('./data.json');
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// ;
+//
+// app.get('favorites', function(req, res){
+//   if(!req.body.name || !req.body.oid){
+//     res.send("Error");
+//     return
+//
+//   var data = JSON.parse(fs.readFileSync('./data.json'));
+//   data.push(req.body);
+//   fs.writeFile('./data.json', JSON.stringify(data));
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
 
-app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
-;
-
-app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
-    res.send("Error");
-    return
-  
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
-});
-
-app.list(3000, function(){
+app.listen(app.get("port"), function(){
   console.log("Listening on port 3000");
 });
