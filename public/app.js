7e5e393eb650fdeadb066a6c016824d62aac11cb
Christine Movius <christine.movius@gmail.com>
2016-04-20 12:43:13 -0400

    Make successful api call to OMDB API to return objects of movie data in console.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,28 @@
 console.log("I'm connected!");
 
-var button = $("#button");
-
-button.on("click", function(){
-  console.log("clicked!");
+$(document).ready(function(){
+  var button = $("#button");
+  button.on("click", function(event){
+    event.preventDefault();
+    console.log("clicked!");
+    apiCall();
+  });
 });
+
+function apiCall() {
+  console.log("Api Call");
+  var keyword = $("input[name='keyword']").val();
+  console.log(keyword);
+  var url = "https://www.omdbapi.com/?t="+keyword;
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json"
+  }).done(function(response){
+    console.log(response);
+  }).fail(function(){
+    console.log("fail");
+  }).always(function(){
+    console.log("Something happens");
+  });
+}
