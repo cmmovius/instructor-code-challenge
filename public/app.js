a5fea3b2a41aea7d6a0eb77456ea484256f905c3
Christine Movius <christine.movius@gmail.com>
2016-04-20 19:53:57 -0400

    start to replace jquery DOM variables with vanilla js.

--- public/app.js
+++ public/app.js
@@ -1,38 +1,36 @@
 console.log("I'm connected!");
 
-$(document).ready(function(){
-  var button = $("#button");
-  button.on("click", function(event){
-    event.preventDefault();
-    apiCall();
-  });
+var button = document.querySelector("#button");
+button.addEventListener("click", function(event){
+  event.preventDefault();
+  apiCall();
 });
 
 function apiCall() {
-  var keyword = $("input[name='keyword']").val();
-  console.log(keyword);
+  var keyword = document.querySelector("input[name='keyword']").value;
   var url = "https://www.omdbapi.com/?t="+keyword;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json"
-  }).done(function(response){
-    console.log(response);
-    showMovie(response);
-  }).fail(function(){
-    console.log("fail");
-  }).always(function(){
-    console.log("Something happens");
-  });
+  console.log(url);
+  // $.ajax({
+  //   url: url,
+  //   type: "GET",
+  //   dataType: "json"
+  // }).done(function(response){
+  //   console.log(response);
+  //   // showMovie(response);
+  // }).fail(function(){
+  //   console.log("fail");
+  // }).always(function(){
+  //   console.log("Something happens");
+  // });
 }
 
-function showMovie(response){
-  var movie = $("#movie");
-  console.log(response.Title);
-  console.log(response.Year);
-  movie.append("<h3>" + response.Title+ "<h3>");
-  movie.append("<img src='"+ response.Poster +"'>");
-  movie.append("<p> Year: " + response.Year+ "<p>");
-  movie.append("<p> Rated: " + response.Rated+ "<p>");
-  movie.append("<p> Actors: " + response.Actors+ "<p>");
-}
+// function showMovie(response){
+//   var movie = $("#movie");
+//   console.log(response.Title);
+//   console.log(response.Year);
+//   movie.append("<h3>" + response.Title+ "<h3>");
+//   movie.append("<img src='"+ response.Poster +"'>");
+//   movie.append("<p> Year: " + response.Year+ "<p>");
+//   movie.append("<p> Rated: " + response.Rated+ "<p>");
+//   movie.append("<p> Actors: " + response.Actors+ "<p>");
+// }
