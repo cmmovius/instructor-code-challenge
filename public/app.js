2c3ba799a3381d1d25b47410ec3adceec976acb2
Christine Movius <christine.movius@gmail.com>
2016-04-20 13:54:54 -0400

    API call returns data about the movie onto the app-welcome page.

--- public/app.js
+++ public/app.js
@@ -4,13 +4,11 @@ $(document).ready(function(){
   var button = $("#button");
   button.on("click", function(event){
     event.preventDefault();
-    console.log("clicked!");
     apiCall();
   });
 });
 
 function apiCall() {
-  console.log("Api Call");
   var keyword = $("input[name='keyword']").val();
   console.log(keyword);
   var url = "https://www.omdbapi.com/?t="+keyword;
@@ -20,9 +18,21 @@ function apiCall() {
     dataType: "json"
   }).done(function(response){
     console.log(response);
+    showMovie(response);
   }).fail(function(){
     console.log("fail");
   }).always(function(){
     console.log("Something happens");
   });
+}
+
+function showMovie(response){
+  var movie = $("#movie");
+  console.log(response.Title);
+  console.log(response.Year);
+  movie.append("<h3>" + response.Title+ "<h3>");
+  movie.append("<img src='"+ response.Poster +"'>");
+  movie.append("<p> Year: " + response.Year+ "<p>");
+  movie.append("<p> Rated: " + response.Rated+ "<p>");
+  movie.append("<p> Actors: " + response.Actors+ "<p>");
 }
