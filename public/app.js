bc11ee183e7f04f014a227398a511542f5e95b05
Christine Movius <christine.movius@gmail.com>
2016-04-21 22:36:42 -0400

    Add some descriptive comments.

--- public/app.js
+++ public/app.js
@@ -1,26 +1,32 @@
 console.log("I'm connected!");
 
+// This variable selects the search button
 var button = document.querySelector("#button");
+//When the search button is clicked, the OMDBAPI is called
 button.addEventListener("click", function(event){
+  //Prevent default is frequently needed on certain event listeners for them to properly follow through with the rest of the function.
   event.preventDefault();
   apiCall();
 });
 
 function apiCall() {
+  //Keyword grabs the value the user puts in the input field.
   var keyword = document.querySelector("input[name='keyword']").value;
+  //We combine this keyword with the rest of our search string.
   var url = "https://www.omdbapi.com/?t="+keyword;
   console.log(url);
+  //OMDBAPI Call
   $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json"
+    url: url, //Sending our url with the keyword search
+    type: "GET", //We are requesting to `get` the information in our query
+    dataType: "json" //We are requesting that the API return the information as JSON data.
   }).done(function(response){
     console.log(response);
-    showMovie(response);
+    showMovie(response); //Upon a successful call, perform this function.
   }).fail(function(){
     console.log("fail");
   }).always(function(){
-    console.log("Something happens");
+    console.log("Something happens"); //We inlcude this to ensure the our API call is still going through, regardless if it is successful or not.
   });
 }
 
@@ -44,5 +50,5 @@ function showMovie(response){
     movie.innerHTML += ("<p> Runtime: " + response.Runtime+ "<p>");
     movie.innerHTML += ("<p> Released: " + response.Released+ "<p>");
     movie.innerHTML += ("<p> IMBD Rating: " + response.imdbRating+ "<p>");
-  })
+  });
 }
